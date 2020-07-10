dabeb01d4ecbde9cd6fd6288c0db79f1dedc8a28
yuyln <joseph.cbs.jabuticabal@gmail.com>
2020-07-10 19:35:07 -0300

    criação da classe bot

--- twitterbot.py
+++ twitterbot.py
@@ -1,15 +1,20 @@
 import os
 from twitter import *
+class Bot(Api):
+    def __init__(self, consumer_key, consumer_secret, access_token_key, access_token_secret):
+        super().__init__(consumer_key, consumer_secret, access_token_key, access_token_secret)
+        self.PostUpdate("teste")
+
+
 
 access_token = os.environ['ACCESS_TOKEN']
 access_secret = os.environ['ACCESS_SECRET']
 twitter_key = os.environ['TWITTER_KEY']
 twitter_secret = os.environ['TWITTER_SECRET']
-api = Api(consumer_key=twitter_key,
+
+api = Bot(consumer_key=twitter_key,
                       consumer_secret=twitter_secret,
                       access_token_key=access_token,
                       access_token_secret=access_secret)
 
-#print(api.VerifyCredentials())
-for i in range(100):
-    print(api.PostDirectMessage(str(i), '3337775116', return_json=True))
+
