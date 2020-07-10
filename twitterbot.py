3c2789b01d0af5f5bba162c5da5d92d476155bec
yuyln <joseph.cbs.jabuticabal@gmail.com>
2020-07-10 04:05:46 -0300

    Cricao

---
+++ twitterbot.py
@@ -0,0 +1,7 @@
+import twitter, os
+
+access_token = os.environ['ACCESS_TOKEN']
+access_secret = os.environ['ACCESS_SECRET']
+twitter_key = os.environ['TWITTER_KEY']
+twitter_secret = os.environ['TWITTER_SECRET']
+print(access_token, access_secret, twitter_key, twitter_secret)
