1e93c9a564199d4cd91cc3931ceacd1359617b25
yuyln <joseph.cbs.jabuticabal@gmail.com>
2020-07-11 02:06:50 -0300

    o bot ira postar hentai

--- twitterbot.py
+++ twitterbot.py
@@ -1,11 +1,39 @@
 import os
 from twitter import *
+import rule34
+from time import sleep
+from random import choice
+import datetime
 
 
 class Bot(Api):
-    def __init__(self, consumer_key, consumer_secret, access_token_key, access_token_secret):
-        super().__init__(consumer_key, consumer_secret, access_token_key, access_token_secret)
-        self.PostUpdate("teste")
+	hentais = ['darkness_(konosuba)',
+			   'tristana',
+			   'vayne']
+	def __init__(self, consumer_key, consumer_secret, access_token_key, access_token_secret, sleep_on_rate_limit=False):
+		super().__init__(consumer_key, consumer_secret, access_token_key, access_token_secret, sleep_on_rate_limit)
+		print("O bot ligou")
+	
+	def posta_rule(self):
+		r34 = rule34.Sync()
+		img = r34.getImages(choice(Bot.hentais))
+		try:
+			img = choice(r34.getImages(choice(Bot.hentais)))
+			img1 = img.file_url
+			img = r34.download(img.file_url)
+			self.PostUpdate(status="", media=img)
+			os.remove(img)
+			with open('log.txt', 'a') as arq:
+				arq.write(f'Postado: {img1}\nData:{datetime.datetime.now().strftime("%H:%M:%S - %d/%m/%Y")}\n---------------------------')		
+		except Exception as e:
+			with open('log.txt', 'a') as arq:
+				arq.write(f'Erro: {e}\nData:{datetime.datetime.now().strftime("%H:%M:%S - %d/%m/%Y")}\n---------------------------')
+				
+			
+			
+		
+		
+        
 
 
 access_token = os.environ['ACCESS_TOKEN']
@@ -17,5 +45,4 @@ api = Bot(consumer_key=twitter_key,
                       consumer_secret=twitter_secret,
                       access_token_key=access_token,
                       access_token_secret=access_secret)
-
-
+api.posta_rule()
